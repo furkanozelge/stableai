c190a4177b0ebd3f0af2adfd6555bcd2bbe1b3ef
Furkan Özelge <furkancoc11@gmail.com>
2023-04-20 01:14:10 +0300

    router

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
 import { Button, Flex, Box } from '@chakra-ui/react'
+import {useRouter} from "next/router"
+
 
 function index() {
+  const router = useRouter();
+  const handleClick = () =>{
+
+    router.push("/sign-in")
+  }
   return (
-    <Box>
+    <Box mb={5}>
       <Flex align="center" bg="black" justifyContent="space-between">
         <Box ml={5}>
         <Image
@@ -14,7 +21,7 @@ function index() {
           height={130}
         />
         </Box>
-        <Button mr={5} color="#000000" bg="#b28afd" size="sm">
+        <Button onClick={handleClick} mr={5} color="#000000" bg="#b28afd" size="sm">
           Start Now
         </Button>
       </Flex>
