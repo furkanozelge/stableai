a97c346dd2b6835e96a96b1927a0310bfe26c8b4
Furkan Özelge <furkancoc11@gmail.com>
2023-08-07 16:47:35 +0300

    Update apiLinks.js

--- utils/apiLinks.js
+++ utils/apiLinks.js
@@ -1,2 +1,2 @@
-export const GENERATE_API = 'https://bce2-34-90-214-51.ngrok-free.app';
-export const JWT_API = 'https://9ba9-178-233-24-227.ngrok-free.app'
+export const GENERATE_API = 'https://8bd9-34-168-112-188.ngrok-free.app';
+export const JWT_API = 'https://9bbf-95-183-240-91.ngrok-free.app'
